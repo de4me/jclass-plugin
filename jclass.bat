15c3168826ac24459ab7835c15dab991c2bf8207
Denis Popov <aoh@list.ru>
2025-04-09 21:58:10 +0300

    Update automatically version string with script parameter;

--- jclass.bat
+++ jclass.bat
@@ -1,18 +1,53 @@
 
 @echo off
 
+
+setlocal
+
+
 set CURRENTDIR=%cd%
+set VERSION=%1
 
-del "%CURRENTDIR%\jclass%1.zip"
 
-7z a -mx9 "%CURRENTDIR%\jclass%1.zip" "%CURRENTDIR%\pluginst.inf"
+if [%VERSION%] == [] (
+  set SUFFIX=
+) else (
+  set SUFFIX=-%VERSION:.=_%
+)
+
+
+set OUTPUTZIP=%CURRENTDIR%\jclass%SUFFIX%.zip
+set OUTPUTINF=%CURRENTDIR%\pluginst.inf
+
+
+if exist "%OUTPUTZIP%" (
+  del /Q "%OUTPUTZIP%"
+)
+
+
+echo [plugininstall] > "%OUTPUTINF%"
+echo description=JClass plugin allows Total Commander to view java .class files. >> "%OUTPUTINF%"
+echo descriptionRus=JClass плагин для Total Commander позволяет просматривать содержимое файлов java .class. >> "%OUTPUTINF%"
+echo type=wlx >> "%OUTPUTINF%"
+echo file=jclass.wlx >> "%OUTPUTINF%"
+echo defaultdir=jclass >> "%OUTPUTINF%"
+
+if "%VERSION%" neq "" (
+  echo version=%VERSION% >> "%OUTPUTINF%"
+)
+
+
+7z a -mx9 "%OUTPUTZIP%" "%OUTPUTINF%"
 
 cd "%CURRENTDIR%\bin\x64\Release\"
 
-7z a -mx9 -w "%CURRENTDIR%/jclass%1.zip" jclass.wlx64
+7z a -mx9 "%OUTPUTZIP%" jclass.wlx64
 
 cd "%CURRENTDIR%\bin\win32\Release\"
 
-7z a -mx9 -w "%CURRENTDIR%/jclass%1.zip" jclass.wlx
+7z a -mx9 "%OUTPUTZIP%" jclass.wlx
 
 cd "%CURRENTDIR%"
+
+
+:exit
